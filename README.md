518f205d0873b218220d50b093d8f3f690b0e2b8
eebaez <16388738+eebaez@users.noreply.github.com>
2026-01-22 16:09:10 -0800

    creating and uploading files for node web server

---
+++ README.md
@@ -0,0 +1,47 @@
+# Node: Web Server
+Accepts HTTP request on port: 3000 and respond with a simple message
+
+## Includes:
+* relevant comments
+* logic to hanlde graceful shutdown
+
+## Technologies
+- Developed using NodeJS v24
+- Using npm v11 for package handling
+- Usign Docker to build image and run as docker container
+
+## Diagram
+
+```mermaid
+    architecture-beta
+        group api(cloud)[Cloud]
+        service server(server)[Web Server] in api
+```
+
+## How-to: Run
+
+Http server will listen for requests here:
+```
+http://localhost:3000/
+```
+
+### Local
+```
+npm run start
+```
+
+### Docker
+Build docker image with a predefined name
+```
+npm run docker-build
+```
+
+Run image as a docker container
+```
+npm run docker-run
+```
+
+Stop container and removes it from local registry
+```
+npm run docker-destroy
+```
---
+++ src/server.js
@@ -0,0 +1,66 @@
+// modules
+const http = require("node:http");
+
+// variables
+const hostname = process.env.HOSTNAME || "localhost";
+const port = process.env.PORT || 3000;
+
+const server = http.createServer();
+
+// helper functions
+const inspectRequest = (request) => {
+    const { method, url, headers } = request;
+    console.log(`${method}, ${url}`);
+    console.log(headers["user-agent"]);
+};
+
+const gracefulShutdown = signal => {
+    console.log(`Received ${signal}. Shutting down gracefully...`);
+    
+    server.close(() => {
+        console.log("Server closed, exisiting process.");
+        process.exit(0); // exit once all connections are closed
+    });
+
+    // closes all connections, ensuring the server closes successfully
+    server.closeAllConnections();
+
+    // force close if cleanup takes too long
+    setTimeout(() => {
+        console.error("Forcing shutdown after 10s timeout.");
+        process.exit(1);
+    }, 10000);
+};
+
+//listener
+server.on("request", (request, response) => {
+    
+    // handle request body
+    let body = [];
+    request.on("error", err => {
+        console.error(err);
+    }).on("data", chunck => {
+        body.push(chunck);
+    }).on("end", () => {
+        body = Buffer.concat(body).toString();
+        // now that the body has arrived and the rest of request info is available then it can be processed.
+
+        response.on("error", err => {
+            console.error(err);
+        });
+
+        inspectRequest(request);
+
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/html");
+        response.end("<html><body><h1>Hello, World!</h1></body></html>");
+    });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// list for termination signals
+process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+process.on("SIGINT", () => gracefulShutdown("SIGINT"));
